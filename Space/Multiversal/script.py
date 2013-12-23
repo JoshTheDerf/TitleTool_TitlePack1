7d6f746ae139afd6a5c4d8d1caad345615f74351
Triangle717 <le717@hotmail.com>
2013-12-23 11:06:02 -0500

    Lint, PEP 8 checks
    Tabs to 4 space indentation,  unchained imports, Blender-only import
    last, line length, spaces around operators, end of file line, proper
    comparison for True

--- Space/Multiversal/script.py
+++ Space/Multiversal/script.py
@@ -1,10 +1,12 @@
-import bpy, sys, os
+import sys
+import os
+import bpy
+
+
 def init(params):
-	bpy.data.objects["Text"].data.body = params['titleText']
-	bpy.data.materials['Material'].diffuse_color = params['color1']
-	bpy.data.materials['Material'].texture_slots[0].color = params['color2']
+    bpy.data.objects["Text"].data.body = params['titleText']
+    bpy.data.materials['Material'].diffuse_color = params['color1']
+    bpy.data.materials['Material'].texture_slots[0].color = params['color2']
 
-	if params['animation'] == True:
-		bpy.ops.render.render(animation=True)
-
-
+    if params['animation']:
+        bpy.ops.render.render(animation=True)
